71785d69d7bd3fe4e0d32dfb346350b363ef4766
KyleF01 <89658838+KyleF01@users.noreply.github.com>
2022-05-02 11:05:18 -0600

    Update script.js

--- new-tab/script.js
+++ new-tab/script.js
@@ -7,7 +7,7 @@ function getWeather() {
     document.querySelector("#weatherDiv").textContent = `${lat},${lon}`
   });
 	const Http = new XMLHttpRequest();
-	const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2513085af99b5a5ddd40889837e7cfc2`;
+	const url=`https://api.openweathermap.org/data/2.5/weather?lat="${lat}"&lon="${lon}"&appid=2513085af99b5a5ddd40889837e7cfc2`;
 	Http.open("GET", url);
 	Http.send();
 	Http.onreadystatechange = (e) => {
